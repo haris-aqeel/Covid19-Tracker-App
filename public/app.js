9f6cd19ffe34fc6548da04944d22b1f51180a00e
haris-aqeel <harisaqeel.2001@gmail.com>
2020-09-01 00:04:27 +0500

    Firebase added

---
+++ public/app.js
@@ -0,0 +1,32 @@
+function handleFormSubmit(e) {
+    e.preventDefault();
+}
+async function handleSearch() {
+    var searchCountry = (document.getElementById('country-search').value);
+    var putValue = searchCountry[0].toUpperCase() + searchCountry.slice(1);
+    const data = await fetch('https://api.covid19api.com/summary');
+    const jsdata = await data.json();
+    const country_names = jsdata.Countries;
+    country_names.forEach(element => {
+        if (element.Country==putValue){
+            console.log(element);
+            insertStats(element);
+            return null;
+        
+        }else{
+            alert('Enter A Valid Country name!')
+        }
+        
+    }); 
+}
+
+function insertStats(details){
+    console.log(details)
+    var patient = document.getElementById('patient');
+    var death = document.getElementById('death');
+    var recovered = document.getElementById('recovered');
+    patient.textContent = details.TotalConfirmed;
+    death.textContent= details.TotalDeaths;
+    recovered.textContent = details.TotalRecovered;
+
+}
